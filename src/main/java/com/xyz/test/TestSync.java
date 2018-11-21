6abd7a5323c9b4b8167a0e345155eb4350a97ab5
xiebinyu <xiebinyu@xdf.cn>
2018-11-21 17:58:11 +0800

    sync同步

--- src/main/java/com/xyz/test/TestSync.java
+++ src/main/java/com/xyz/test/TestSync.java
@@ -10,12 +10,9 @@ public class TestSync extends Thread {
 
   @Override
   public void run() {
+    // 此处只能try catch，因为自Thread基类继承的该方法没有throws
     try {
-      switch (type) {
-        case 1: person.sleep(); break;
-        case 2: person.breath(); break;
-        case 3: person.eat(); break;
-      }
+      person.run(type);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -33,11 +30,11 @@ public class TestSync extends Thread {
 }
 
 class Person {
-  Integer i = 1;
+  Integer key = 1;
 
-  // 同步代码块，如果同步变量是this，则锁对象，当前锁的是i，其它同步方法不受影响
+  // 同步代码块，如果同步变量是this，则锁对象，当前锁的是key，其它同步方法不受影响
   public void sleep() throws Exception {
-    synchronized(i) {
+    synchronized(key) {
       Thread.sleep(3000);
       System.out.println("睡觉！");
     }
@@ -51,5 +48,13 @@ class Person {
   // 同步方法，导致其它同步方法不能访问，非同步方法不受影响
   public synchronized void eat() {
     System.out.println("吃饭！");
+  }
+
+  public void run(int type) throws Exception {
+    switch (type) {
+      case 1: sleep(); break;
+      case 2: breath(); break;
+      case 3: eat(); break;
+    }
   }
 }
